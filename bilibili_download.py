c16eab53557d7d7b0671fe1d00501c29d064b27a
amenonioi <139857159+amenonioi@users.noreply.github.com>
2025-07-31 00:57:33 +0800

    Update bilibili_download.py

--- bilibili_download.py
+++ bilibili_download.py
@@ -37,7 +37,7 @@ print(f'{RESET}')
 while True:
     instruction = questionary.select(
         "请选择指令 (箭头切换，ENTER确认):",
-        choices=["进行下载", "改变保存位置", "登录b站账号（使用sessdata，获取方法详见https://github.com/amenonioi/Bilibili_Download）", "退出程序"],
+        choices=["进行下载", "改变保存位置", "登录b站账号（使用sessdata，获取方法详见https://github.com/amenonioi/Bilibili_Download）", "更改默认设置", "退出程序"],
         default="进行下载"
     ).ask()
     if instruction == '退出程序':
@@ -57,7 +57,7 @@ while True:
         else:
             print('无效的路径')
         print(f'\n{RESET}', end='')
-    elif instruction == '登录b站账号（使用sessdata）':
+    elif instruction == '登录b站账号（使用sessdata，获取方法详见https://github.com/amenonioi/Bilibili_Download）':
         sessdata = questionary.text("请输入sessdata:").ask()
         cookies = {'SESSDATA': sessdata}
         user_info['SESSDATA'] = sessdata
@@ -74,11 +74,46 @@ while True:
             print('游客状态')
         else:
             vip = info['data']['vipType']
-            if vip == 0:
+            if info['data']['vipStatus'] == 0:
                 print('普通用户')
             elif vip == 1:
                 print('大会员')
             elif vip == 2:
                 print('年度大会员')
         print(f'\n{RESET}', end='')
+    elif instruction == '更改默认设置':
+        choices = ["是否下载重名文件", "是否将所有视频相关文件存储于文件夹中"]
+        if user_info['download_duplicate_file']:
+            choices[0] += " （当前选择为【是】）"
+        else:
+            choices[0] += " （当前选择为【否】）"
+        if user_info['save_in_file']:
+            choices[1] += " （当前选择为【是】）"
+        else:
+            choices[1] += " （当前选择为【否】）"
+
+        instruction = questionary.select(
+            "请选择更改内容 (箭头切换，ENTER确认):",
+            choices=choices
+        ).ask()
+        if instruction == choices[0]:
+            instruction = questionary.select(
+                "是否下载重名文件 (箭头切换，ENTER确认):",
+                choices=["是", "否"]
+            ).ask()
+            if instruction == "是":
+                user_info['download_duplicate_file'] = True
+            else:
+                user_info['download_duplicate_file'] = False
+        if instruction == choices[1]:
+            instruction = questionary.select(
+                "是否下载重名文件 (箭头切换，ENTER确认):",
+                choices=["是", "否"]
+            ).ask()
+            if instruction == "是":
+                user_info['save_in_file'] = True
+            else:
+                user_info['save_in_file'] = False
+        with open('user_data.json', 'w', encoding='utf-8') as user_data:
+            user_data.write(json.dumps(user_info))
 
